9ea5f0b33c9ead26502ce4760695bd102f00830b
madhupriyakadali <94887276+madhupriyakadali@users.noreply.github.com>
2021-12-28 19:21:50 +0530

    Delete Department.java

--- Department.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Department{
-void print(int DepId,String DepName,String DepHead)
-{
-System.out.println("Department id is:"+DepId);
-System.out.println("Department name is:"+DepName);
-System.out.println("Department head is:"+DepHead);
-}
-}
