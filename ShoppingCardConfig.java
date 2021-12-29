f88b7f3e169f50d005b0b6b7eac8478cf3293966
madhupriyakadali <94887276+madhupriyakadali@users.noreply.github.com>
2021-12-29 19:19:14 +0530

    Add files via upload

---
+++ ShoppingCardConfig.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class ShoppingCardConfig 
+{
+ @Bean
+  public ShoppingCard shoppingCard()
+  {
+	 return new ShoppingCard("HDFC",12345); 
+  }
+  @Bean
+  public Item item()
+  {
+	 return new Item("IceCream",100); 
+  }
+}
